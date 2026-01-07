e7096d5015f4cbdf20443ce191b75574688b09b5
Sanjiv99kv <vishsanjeev99@gmail.com>
2026-01-07 16:08:42 +0530

    fixed the margin of hero section

--- src/components/homeSection/HeroSection.jsx
+++ src/components/homeSection/HeroSection.jsx
@@ -1,6 +1,6 @@
 export default function HeroSection() {
     return (
-        <div className="md:h-[420px] h-[510px] md:mb-10 mb-5">
+        <div className="md:h-[420px] h-[510px] md:mb-10 mb-5 mt-26 md:mt-0">
             <img src="/images/heroSection.png" alt="heroSection" className="w-full h-full object-cover hidden md:block" />
             <img src="/images/heroSectionMobile.webp" alt="heroSection" className="h-full w-full object-cover block md:hidden" />
         </div>
